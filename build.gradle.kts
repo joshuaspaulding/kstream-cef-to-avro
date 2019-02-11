5fa28ba71abd01c341420630a1b612f6c3e52144
Josh Spaulding <josh@alienonesecurity.com>
2019-02-11 13:18:22 -0600

    fixed gradle build with .gitignore

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,3 @@
-
 plugins {
     java
 }
@@ -8,16 +7,10 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
-    maven(url="https://packages.confluent.io/maven/")
 }
 
 dependencies {
-    compile("org.apache.avro:avro:1.8.2")
-    compile("org.apache.kafka:kafka-streams:1.1.0")
-    compile("io.confluent:kafka-avro-serializer:4.1.1")
-    compile("io.confluent:kafka-streams-avro-serde:4.1.1")
-    compile("com.google.guava:guava:27.0.1-jre")
-    compile("org.apache.commons:commons-lang3:3.0")
+    compile("org.apache.kafka:kafka-streams:2.1.0")
     testCompile("junit", "junit", "4.12")
 }
 
@@ -32,7 +25,7 @@ configure<JavaPluginConvention> {
 val fatJar = task("fatJar", type = Jar::class) {
     baseName = "${project.name}-fat"
     manifest {
-        attributes["Main-Class"] = "VF18.Application"
+        attributes["Main-Class"] = "StreamsStarterApp"
     }
     from(configurations.runtime.map({ if (it.isDirectory) it else zipTree(it) }))
     with(tasks["jar"] as CopySpec)
